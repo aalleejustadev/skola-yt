48de2b6ddfbc052c19416b9f6832c4e032c7dd6e
Ali Murtaza <ali@badoatech.com>
2023-10-20 19:00:38 +0500

    completed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', function () {
+	html = document.querySelector('html');
 	// Header search trigger
 	if (document.querySelector('.search-trigger')) {
 		const searchTrigger = document.querySelector('.search-trigger');
@@ -32,6 +33,15 @@ window.addEventListener('load', function () {
 			perMove: 1,
 			pagination: false,
 			gap: '30px',
+			mediaQuery: 'max',
+			breakpoints: {
+				820: {
+					perPage: 3,
+				},
+				576: {
+					perPage: 2,
+				},
+			},
 		}).mount();
 	}
 
@@ -42,6 +52,30 @@ window.addEventListener('load', function () {
 			perMove: 4,
 			arrows: false,
 			gap: '30px',
+			mediaQuery: 'max',
+			breakpoints: {
+				1024: {
+					perPage: 3,
+					perMove: 3,
+				},
+				820: {
+					perPage: 2,
+					perMove: 2,
+				},
+				576: {
+					perPage: 1.5,
+					perMove: 1,
+				},
+			},
 		}).mount();
+	}
+
+	// Mobile navigation
+	if (document.querySelector('.navigation')) {
+		const menuTrigger = document.querySelector('.menu-trigger');
+		// const navigation = document.querySelector('.navigation');
+		menuTrigger.addEventListener('click', function () {
+			html.classList.toggle('menu-opened');
+		});
 	}
 });
